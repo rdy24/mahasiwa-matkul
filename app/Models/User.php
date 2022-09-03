b08ec0ab06ebed2ad8993bc287cdf8d608f0b684
rdy24 <raflizardeswandiy24@gmail.com>
2022-09-03 17:30:30 +0700

    edit model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $with = ['mahasiswa'];
+    // protected $with = ['mahasiswa'];
 
     protected $fillable = [
         'email', 'password',
